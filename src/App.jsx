aeeacec91f8036f25f17a0151af34bb73cafd575
samubj <bjsamukalavathy@gmail.com>
2026-07-13 16:41:17 +0530

    Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,29 @@ import ForgotPassword from "./Components-LoginPage/ForgotPassword";
 import CheckEmail from "./Components-LoginPage/CheckEmail";
 import RegistrationPage from "./Components-RegistrationPage/RegistrationPage";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <LandingPage />,
+    },
+    {
+      path: "/forgot-password",
+      element: <ForgotPassword />,
+    },
+    {
+      path: "/check-email",
+      element: <CheckEmail />,
+    },
+    {
+      path: "/register",
+      element: <RegistrationPage />,
+    },
+  ],
   {
-    path: "/",
-    element: <LandingPage />,
-  },
-  {
-    path: "/forgot-password",
-    element: <ForgotPassword />,
-  },
-  {
-    path: "/check-email",
-    element: <CheckEmail />,
-  },
-  {
-    path: "/register",
-    element: <RegistrationPage />,
-  },
-]);
+    basename: "/PRP_Portal",
+  }
+);
 
 function App() {
   return <RouterProvider router={router} />;
